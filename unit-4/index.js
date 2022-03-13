901cfc98b4c60b01d18896d436bf27c281e0fe39
sharan <sharanm766@gmail.com>
2022-03-13 18:37:50 +0530

    https://github.com/sharan96632/unit4c1

--- unit-4/index.js
+++ unit-4/index.js
@@ -1,1 +1,16 @@
-console.log("hii")
+const express= require("express");
+const mongoose=require("mongoose");
+
+const app=express();
+
+const connect= () =>{
+return mongoose.connect(
+    "mongodb+srv://sharan1:sharan123@cluster0.s30rh.mongodb.net/test"
+);
+};
+
+app.listen(5000,async()=>{
+    await connect();
+
+    console.log("listening on port 5000")
+})
